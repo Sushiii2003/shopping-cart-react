f739404af7c33f127a2c98e4e36ae3f7682cae2e
Sushiii2003 <sushantkumar0704@gmail.com>
2024-07-22 12:11:42 +0530

    i changed the e-comm

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export const Contact = () => {
-  return <div>contact</div>;
+  return <><div><strong>Contact Us</strong></div>
+  <div><strong>E-mail</strong>: <em>ecommtech@gmail.com</em></div>
+  <div><strong>Tel</strong>: <em>9994783732</em></div></>
 };
